d646a49bb4fd312c64ba6f785717194a38f4907d
Br1ght3stDay117 <alejandroalvarez1997@hotmail.com>
2018-07-04 18:37:32 -0500

    V2

--- Eneger/settings.py
+++ Eneger/settings.py
@@ -50,7 +50,7 @@ MIDDLEWARE = [
     'django.middleware.clickjacking.XFrameOptionsMiddleware',
 ]
 
-ROOT_URLCONF = 'Recneger.urls'
+ROOT_URLCONF = 'Eneger.urls'
 
 TEMPLATES = [
     {
@@ -68,7 +68,7 @@ TEMPLATES = [
     },
 ]
 
-WSGI_APPLICATION = 'Recneger.wsgi.application'
+WSGI_APPLICATION = 'Eneger.wsgi.application'
 
 
 # Database
